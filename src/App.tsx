b7fff0e7a116bcb6d3aed8aff048a31b8daca0ce
chimpaji <chimpaji@outlook.com>
2022-10-17 00:22:24 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { APP_ID, CHANNEL_NAME, TEMP_TOKEN } from './settings';
+
+import { useState } from 'react';
+
+function App() {
+  return <div className='App'>asdfsdf</div>;
+}
+
+export default App;
---
+++ src/settings.ts
@@ -0,0 +1,3 @@
+export const APP_ID = import.meta.env.VITE_APP_ID;
+export const CHANNEL_NAME = import.meta.env.VITE_CHANNEL_NAME;
+export const TEMP_TOKEN = import.meta.env.VITE_TEMP_TOKEN;
